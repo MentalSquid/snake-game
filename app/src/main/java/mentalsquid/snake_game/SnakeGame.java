69f7df7df070314dc4431c91ff5455b8e610a2c9
MentalSquid-SEVS17011 <maliqnor+gitSEVS17011@gmail.com>
2018-12-10 11:55:37 +0100

    push android studio project

---
+++ app/src/main/java/mentalsquid/snake_game/SnakeGame.java
@@ -0,0 +1,12 @@
+package mentalsquid.snake_game;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SnakeGame extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

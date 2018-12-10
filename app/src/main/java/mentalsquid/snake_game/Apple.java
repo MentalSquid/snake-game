abbb0295a4e1dfabc6b573a1893b876f5b1ffcbd
MentalSquid-Ampere <maliqnor+gitAmpere@gmail.com>
2018-12-10 15:28:40 +0100

    merge old code and assets

--- app/src/main/java/mentalsquid/snake_game/Apple.java
+++ app/src/main/java/mentalsquid/snake_game/Apple.java
@@ -1,4 +1,42 @@
 package mentalsquid.snake_game;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+
+import java.util.Random;
+
 public class Apple {
+
+    private Point mLocation = new Point();
+    private Point mSpawnRange;
+    private int mSize;
+    private Bitmap mBitmapApple;
+
+    public Apple(Context context, Point sr, int s) {
+        mSpawnRange = sr;
+        mSize = s;
+        //place the apple off screen until the game starts.
+        mLocation.x = -10;
+
+        mBitmapApple = BitmapFactory.decodeResource(context.getResources(), R.drawable.apple);
+        mBitmapApple = Bitmap.createScaledBitmap(mBitmapApple, s, s, false);
+    }
+
+    void spawn() {
+        Random random = new Random();
+        mLocation.x = random.nextInt(mSpawnRange.x) + 1;
+        mLocation.y = random.nextInt(mSpawnRange.y - 1) + 1;
+    }
+
+    Point getLocation() {
+        return mLocation;
+    }
+
+    void draw(Canvas canvas, Paint paint) {
+        canvas.drawBitmap(mBitmapApple, mLocation.x * mSize, mLocation.y * mSize, paint);
+    }
 }
--- app/src/main/java/mentalsquid/snake_game/SnakeGame.java
+++ app/src/main/java/mentalsquid/snake_game/SnakeGame.java
@@ -1,12 +1,157 @@
 package mentalsquid.snake_game;
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
-public class SnakeGame extends Activity {
+import java.io.IOException;
+
+public class SnakeGame extends SurfaceView implements Runnable {
+    private int mScreenX;
+    private int mScreenY;
+    private Thread mGameThread = null;
+    private long mNextFrameTime;
+    private volatile boolean mPlaying = false;
+    private volatile boolean mPaused = true;
+    private SoundPool mSP;
+    private int mEat_ID = -1;
+    private int mCrashID = -1;
+    private final int NUM_BLOCKS_WIDE = 40;
+    private int mNumBlocksHigh;
+    private int mScore;
+    private Canvas mCanvas;
+    private SurfaceHolder mSurfaceHolder;
+    private Paint mPaint;
+    private Snake mSnake;
+    private Apple mApple;
+
+    public SnakeGame(Context context, Point size) {
+        super(context);
+
+        int blockSize = size.x / NUM_BLOCKS_WIDE;
+        mNumBlocksHigh = size.y / blockSize;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            AudioAttributes audioAttributes = new AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_MEDIA).setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION).build();
+
+            mSP = new SoundPool.Builder().setMaxStreams(5).setAudioAttributes(audioAttributes).build();
+        } else {
+            mSP = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+        }
+        try {
+            AssetManager assetManager = context.getAssets();
+            AssetFileDescriptor descriptor;
+
+            descriptor = assetManager.openFd("get_apple.ogg");
+            mEat_ID = mSP.load(descriptor, 0);
+
+            descriptor = assetManager.openFd("snake_death.ogg");
+            mCrashID = mSP.load(descriptor, 0);
+        } catch (IOException e) {
+            Log.e("IOException:", e.getMessage());
+        }
+        mSurfaceHolder = getHolder();
+        mPaint = new Paint();
+        mApple = new Apple(context, new Point(NUM_BLOCKS_WIDE, mNumBlocksHigh), blockSize);
+    }
+
+    public void newGame() {
+        mApple.spawn();
+        mScore = 0;
+        mNextFrameTime = System.currentTimeMillis();
+    }
+
+    public void resume() {
+        mPlaying = true;
+        mGameThread = new Thread(this);
+        mGameThread.start();
+    }
+
+    public void pause() {
+        mPlaying = false;
+        try {
+            mGameThread.join();
+
+        } catch (InterruptedException e) {
+            Log.e("Error: ", e.getMessage());
+        }
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void run() {
+        while (mPlaying) {
+            if (!mPaused) {
+                if (updateRequired()) {
+                    update();
+                }
+            }
+            draw();
+        }
+    }
+
+    private void draw() {
+        if (mSurfaceHolder.getSurface().isValid()) {
+            mCanvas = mSurfaceHolder.lockCanvas();
+
+            //fill the screen with a color
+            mCanvas.drawColor(Color.argb(255, 26, 128, 182));
+            mPaint.setColor(Color.argb(255, 255, 255, 255));
+            mPaint.setTextSize(120);
+            mCanvas.drawText("" + mScore, 20, 120, mPaint);
+            mApple.draw(mCanvas, mPaint);
+
+            if (mPaused) {
+                mPaint.setColor(Color.argb(255, 255, 255, 255));
+                mPaint.setTextSize(250);
+                mCanvas.drawText(getResources().getString(R.string.tap_to_play), 200, 700, mPaint);
+
+            }
+            mSurfaceHolder.unlockCanvasAndPost(mCanvas);
+        }
+    }
+
+    private void update() {
+
+    }
+
+    public boolean updateRequired() {
+        final long TARGET_FPS = 10;
+        final long MILLIS_PER_SECOND = 1000;
+
+        if (mNextFrameTime <= System.currentTimeMillis()) {
+            mNextFrameTime = System.currentTimeMillis() + MILLIS_PER_SECOND / TARGET_FPS;
+
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_UP:
+                if (mPaused) {
+                    mPaused = false;
+                    newGame();
+
+                    return true;
+                }
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
